82c8883b77e1589af91e27b0280ee11f54c999f8
Christopher Hertel <christopher.hertel@sensiolabs.de>
2019-11-20 17:04:38 +0100

    final search feature and navi links

---
+++ src/Controller/SearchController.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use App\Repository\ProductRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SearchController extends AbstractController
+{
+    /**
+     * @Route("/search", name="search", methods={"GET"})
+     */
+    public function search(ProductRepository $repository, Request $request): Response
+    {
+        $query = $request->query->get('query', '');
+
+        return $this->render('search.html.twig', [
+            'paginator' => $repository->search($query),
+            'query' => $query,
+        ]);
+    }
+}
