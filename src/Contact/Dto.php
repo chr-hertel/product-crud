eb499680bc05e83b118e17851bf079fa06f1b1c1
Christopher Hertel <christopher.hertel@sensiolabs.de>
2019-11-20 10:40:22 +0100

    solving contact 2

--- src/Contact/Dto.php
+++ src/Contact/Dto.php
@@ -4,10 +4,31 @@ declare(strict_types=1);
 
 namespace App\Contact;
 
+use Symfony\Component\Validator\Constraints as Assert;
+
 class Dto
 {
+    /**
+     * @Assert\NotBlank
+     * @Assert\Length(min=2)
+     */
     public $name;
+
+    /**
+     * @Assert\NotBlank
+     * @Assert\Email
+     */
     public $email;
+
+    /**
+     * @Assert\NotBlank
+     * @Assert\Length(min=4, max=30)
+     */
     public $subject;
+
+    /**
+     * @Assert\NotBlank
+     * @Assert\Length(min=10, max=300)
+     */
     public $message;
 }
